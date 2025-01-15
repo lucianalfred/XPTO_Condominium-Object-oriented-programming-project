e09eba60173de465985ceda3db0eb6e0ed19dc83
Luciano Alfredo <lucianalfred60@gmail.com>
2025-01-15 21:53:53 +0100

    Proprietario

---
+++ Models/Proprietario.java
@@ -0,0 +1,76 @@
+
+
+package Models;
+import java.time.LocalDate;
+
+public class Proprietario {
+   private String identificador;
+   private String nome;
+   private String morada;
+   private String telefone;
+   private String email;
+   private LocalDate dataNascimento;
+   
+   //Constructor
+
+    public Proprietario(String identificador, String nome, String morada, String telefone, String email, LocalDate dataNascimento) {
+        this.identificador = identificador;
+        this.nome = nome;
+        this.morada = morada;
+        this.telefone = telefone;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+    }
+   
+   //Setters and Getters
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    // outros metodos 
+}
