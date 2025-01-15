422ebd8a9bf1b95bf90d994260e4d34337aef17b
Luciano Alfredo <lucianalfred60@gmail.com>
2025-01-15 21:01:46 +0100

    Classe Modelo do Condomini

--- Models/Condominio.java
+++ Models/Condominio.java
@@ -1,13 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Models;
 
-/**
- *
- * @author lucian
- */
+import java.util.*;
+import java.time.LocalDate;
+
 public class Condominio {
+    
+    private String identifcador;
+    private String morada;
+    private double totalDispesasGerais;
+    private LocalDate dataConstrucaoa;
+    private ArrayList<Fraccao> fraccoes;
+    
+    //Constructor
+
+    public Condominio(String identifcador, String morada, double totalDispesasGerais, LocalDate dataConstrucaoa, ArrayList<Fraccao> fraccoes) {
+        this.identifcador = identifcador;
+        this.morada = morada;
+        this.totalDispesasGerais = totalDispesasGerais;
+        this.dataConstrucaoa = dataConstrucaoa;
+        this.fraccoes = fraccoes;
+    }
+    
+    
+    
+    //get and setters
+
+    public String getIdentifcador() {
+        return identifcador;
+    }
+
+    public void setIdentifcador(String identifcador) {
+        this.identifcador = identifcador;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    public double getTotalDispesasGerais() {
+        return totalDispesasGerais;
+    }
+
+    public void setTotalDispesasGerais(double totalDispesasGerais) {
+        this.totalDispesasGerais = totalDispesasGerais;
+    }
+
+    public LocalDate getDataConstrucaoa() {
+        return dataConstrucaoa;
+    }
+
+    public void setDataConstrucaoa(LocalDate dataConstrucaoa) {
+        this.dataConstrucaoa = dataConstrucaoa;
+    }
+
+    public ArrayList<Fraccao> getFraccoes() {
+        return fraccoes;
+    }
+
+    public void setFraccoes(ArrayList<Fraccao> fraccoes) {
+        this.fraccoes = fraccoes;
+    }
     
 }
